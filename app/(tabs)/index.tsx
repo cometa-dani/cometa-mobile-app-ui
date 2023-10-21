d9f1847a7c73226db4ae1ddf4520395bdec30e15
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-21 00:06:24 -0500

    ✨ scrolling images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,51 @@
-import { useEffect } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+/* eslint-disable react-native/no-color-literals */
+import { useEffect, useState } from 'react';
+import { StyleSheet, Image, DimensionValue } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { useCometaStore } from '../../store/cometaStore';
+import { FlatList } from 'react-native-gesture-handler';
 
 
 export default function HomeScreen(): JSX.Element {
   const { data } = useCometaStore(state => state.events);
   const fetchEvents = useCometaStore(state => state.fetchEvents);
+  const [layoutHeight, setLayoutHeight] = useState<DimensionValue>('100%');
 
   useEffect(() => {
     fetchEvents().then();
   }, []);
 
-  useEffect(() => {
-    console.log(`data: ${JSON.stringify(data)}`);
-  }, [data]);
-
   return (
-    <ScrollView contentContainerStyle={styles.scrollView}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Cometa App</Text>
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        onLayout={(e) => setLayoutHeight(e.nativeEvent.layout.height)}
+        data={data.events}
+        contentContainerStyle={styles.flatListContent}
+        renderItem={({ item }) => (
+          <View style={{
+            alignItems: 'center',
+            backgroundColor: '#eee',
+            height: layoutHeight,
+            justifyContent: 'center',
+            width: '100%',
+          }}>
+            <Image
+              source={{ uri: item.mediaUrl }}
+              style={{ width: '100%', height: '100%' }}
+            />
+            {/* <Text>{item.name}</Text> */}
+          </View>
+        )}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center'
-  },
-  scrollView: {
     flex: 1,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
+  flatListContent: {
+    flexGrow: 1,
+  }
 });
