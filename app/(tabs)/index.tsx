fd60e06eccd317e89f024cd4c0332d96d4645b00
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-21 01:06:43 -0500

    ✨

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable react-native/no-color-literals */
 import { useEffect, useState } from 'react';
-import { StyleSheet, Image, DimensionValue } from 'react-native';
+import { StyleSheet, Image, DimensionValue, Pressable, useColorScheme } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { useCometaStore } from '../../store/cometaStore';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { FontAwesome } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 
 export default function HomeScreen(): JSX.Element {
+
+  const colorScheme = useColorScheme() ?? 'light';
   const { data } = useCometaStore(state => state.events);
   const fetchEvents = useCometaStore(state => state.fetchEvents);
   const [layoutHeight, setLayoutHeight] = useState<DimensionValue>('100%');
@@ -37,6 +42,33 @@ export default function HomeScreen(): JSX.Element {
           </View>
         )}
       />
+
+      <View style={styles.positionedButtons}>
+        <Link href="/modal" asChild>
+          <Pressable>
+            {({ pressed, hovered }) => (
+              <FontAwesome
+                name="heart"
+                size={54}
+                color={'red'}
+              // style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+              />
+            )}
+          </Pressable>
+        </Link>
+        {/* <TouchableOpacity>
+          <FontAwesome name='heart' size={54} style={{ color: 'red' }} />
+        </TouchableOpacity> */}
+        <TouchableOpacity>
+          <FontAwesome name='commenting' size={54} style={{ color: 'red' }} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FontAwesome name='send' size={54} style={{ color: 'red' }} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FontAwesome name='chevron-down' size={54} style={{ color: 'red' }} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -44,8 +76,16 @@ export default function HomeScreen(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position: 'relative',
   },
   flatListContent: {
     flexGrow: 1,
+  },
+  positionedButtons: {
+    backgroundColor: 'transparent',
+    bottom: 44,
+    gap: 18,
+    position: 'absolute',
+    right: 24
   }
 });
