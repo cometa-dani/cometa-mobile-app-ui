e9cec6f1b83508a66df9a5c6679f4ecffbf689f5
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-22 06:39:12 -0500

    💡 better comments

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-native/no-color-literals */
 import { useEffect, useState } from 'react';
 import { StyleSheet, Image, DimensionValue, Pressable } from 'react-native';
 import { Text, View, useColors } from '../../components/Themed';
@@ -9,16 +8,17 @@ import { FontAwesome } from '@expo/vector-icons';
 
 export default function HomeScreen(): JSX.Element {
   const { red100, tabIconDefault } = useColors();
-  const { data } = useCometaStore(state => state.events);
+  const { data } = useCometaStore(state => state.events); // list of events to render
   const fetchEvents = useCometaStore(state => state.fetchEvents);
-  const [layoutHeight, setLayoutHeight] = useState<DimensionValue>('100%');
+  const [layoutHeight, setLayoutHeight] = useState<DimensionValue>('100%'); // sets the height of every item
 
   useEffect(() => {
-    fetchEvents().then();
+    fetchEvents().then(); // fetches latest events from backend
   }, []);
 
   return (
     <View style={styles.container}>
+      {/* latest events */}
       <FlatList
         pagingEnabled={true}
         onLayout={(e) => setLayoutHeight(e.nativeEvent.layout.height)}
@@ -27,17 +27,21 @@ export default function HomeScreen(): JSX.Element {
         renderItem={({ item }) => (
           <View style={{
             alignItems: 'center',
-            backgroundColor: '#eee',
+            // backgroundColor: '#eee',
             height: layoutHeight,
             justifyContent: 'center',
             width: '100%',
           }}>
+            {/* background image */}
             <Image
               source={{ uri: item.mediaUrl }}
               style={{ width: '100%', height: '100%' }}
             />
+            {/* event title */}
             <Text lightColor='#fff' darkColor='#eee' style={styles.title}>{item.name}</Text>
-            <View style={styles.positionedButtons}>
+
+            {/* positioned buttons  */}
+            <View lightColor='transparent' darkColor='transparent' style={styles.positionedButtons}>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome name='heart' size={46} style={{ color: pressed ? red100 : tabIconDefault }} />
@@ -59,8 +63,9 @@ export default function HomeScreen(): JSX.Element {
                 )}
               </Pressable>
             </View>
+            {/* positioned buttons /*}
 
-
+            {/* organizer icon */}
             <View lightColor='transparent' darkColor='transparent' style={styles.organizerContainer}>
               <Image style={styles.img} source={require('@assets/images/asian-cup.jpeg')} />
               <Text lightColor='#fff' darkColor='#eee' style={styles.organizer}>Asian Cup</Text>
@@ -68,6 +73,7 @@ export default function HomeScreen(): JSX.Element {
           </View>
         )}
       />
+      {/* latest events */}
     </View>
   );
 }
@@ -98,7 +104,6 @@ const styles = StyleSheet.create({
     position: 'absolute'
   },
   positionedButtons: {
-    backgroundColor: 'transparent',
     bottom: 30,
     gap: 20,
     position: 'absolute',
