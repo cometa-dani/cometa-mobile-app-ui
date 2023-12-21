37f3179a363ab0321a25387396f888410fa883ea
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-12-21 13:46:04 -0500

    ✨ performance improved

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,13 +1,11 @@
-/* eslint-disable no-unused-vars */
-import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { LikedEvent, CreateEventLike } from '../../models/Event';
-import { StyleSheet, Image, DimensionValue, Pressable, SafeAreaView, Dimensions, ViewToken } from 'react-native';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { LikedEvent, } from '../../models/Event';
+import { StyleSheet, Image, DimensionValue, Pressable, SafeAreaView, ViewToken } from 'react-native';
 import { Text, View, useColors } from '../../components/Themed';
 import { GestureDetector, Gesture, FlatList, Directions } from 'react-native-gesture-handler';
 import { FontAwesome } from '@expo/vector-icons';
-import { router, useFocusEffect } from 'expo-router';
+import { router, } from 'expo-router';
 import { useInfiniteQueryGetLatestEvents, useMutationLikeOrDislikeEvent } from '../../queries/eventHooks';
-import { UseMutationResult } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
 import { useCometaStore } from '../../store/cometaStore';
 import { ResizeMode, Video } from 'expo-av';
@@ -19,35 +17,28 @@ export default function HomeScreen(): JSX.Element {
 
   const onViewRef = useRef(({ viewableItems }: { viewableItems: ViewToken[] }) => {
     if (viewableItems.length > 0) {
+      if (viewableItems[0].item.mediaType === 'VIDEO') {
+
+        console.log('viewableItems', viewableItems[0].item?.id);
+        setPlayingVideo(viewableItems[0].item?.id);
+      }
       // TODO: FIXE temporary fix to avoid playing multiple videos at the same time
-      console.log('viewableItems', viewableItems[0].item?.id);
-      setPlayingVideo(viewableItems[0].item?.id);
-    } else {
-      setPlayingVideo('');
     }
   });
 
-  // Get access to colors and store data
-  const { red100, tabIconDefault } = useColors();
-
-  // global state
-  const { setToggleActionSheet, setLikedEvent } = useCometaStore(state => state);
-
   // events & function to handle fetching more events when reaching the end
   const { data, isFetching, fetchNextPage, hasNextPage } = useInfiniteQueryGetLatestEvents();
-  const handleInfititeFetch = () => !isFetching && hasNextPage && fetchNextPage();
 
-  // perform mutations
-  const likeOrDislikeMutation = useMutationLikeOrDislikeEvent();
+
+  const handleInfiniteFetch = useCallback(() => {
+    if (!isFetching && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [isFetching, hasNextPage, fetchNextPage]);
+
 
   // State variables to manage page and item heights
   const [layoutHeight, setLayoutHeight] = useState<DimensionValue>('100%');
-
-
-  const onShowEventDetails = useCallback((item: LikedEvent): void => {
-    setLikedEvent(item);
-    setToggleActionSheet(true);
-  }, [setLikedEvent, setToggleActionSheet]);
 
 
   return (
@@ -70,18 +61,14 @@ export default function HomeScreen(): JSX.Element {
           pagingEnabled={true}
           data={data?.pages.flatMap(page => page.events)}
           contentContainerStyle={styles.flatListContent}
-          onEndReached={handleInfititeFetch}
+          onEndReached={handleInfiniteFetch}
           onEndReachedThreshold={1}
           renderItem={({ item }) => (
             <MemoizedEventItem
-              showDetails={onShowEventDetails}
-              likeOrDislikeMutation={likeOrDislikeMutation}
               key={item.id}
               item={item}
               playingVideo={playingVideo}
               layoutHeight={layoutHeight}
-              red100={red100}
-              tabIconDefault={tabIconDefault}
             />
           )}
         />
@@ -145,21 +132,31 @@ const styles = StyleSheet.create({
 
 // Define the props for the memoized list item
 interface ListItemProps {
-  showDetails: (item: LikedEvent) => void,
   item: LikedEvent,
   playingVideo: string,
   layoutHeight: DimensionValue,
-  red100: string,
-  tabIconDefault: string,
-  likeOrDislikeMutation: UseMutationResult<CreateEventLike | null, Error, number, void>
 }
 
-const EventItem: FC<ListItemProps> = ({ playingVideo, item, showDetails, likeOrDislikeMutation, layoutHeight, red100, tabIconDefault }) => {
-
+const EventItem: FC<ListItemProps> = ({ playingVideo, item, layoutHeight }) => {
   // video
-  const video = React.useRef<Video>(null);
-  // const videoShouldPlay = useMemo(() => playingVideo === item.id.toString(), [playingVideo]);
-  const videoShouldPlay = playingVideo === item.id.toString();
+  const videoRef = React.useRef<Video>(null);
+  const videoShouldPlay: boolean = playingVideo === item.id.toString();
+
+  // Get access to colors and store data
+  const { red100, tabIconDefault } = useColors();
+
+  // global state
+  const { setToggleActionSheet, setLikedEvent } = useCometaStore(state => state);
+
+  const showEventDetails = (item: LikedEvent): void => {
+    setLikedEvent(item);
+    setToggleActionSheet(true);
+  };
+
+  // perform mutations
+  const likeOrDislikeMutation = useMutationLikeOrDislikeEvent();
+  const handleLikeOrDislike = () => likeOrDislikeMutation.mutate(item.id);
+
 
   // can be lifted to the parent component like before
   const swipeLeft = Gesture.Fling();
@@ -174,17 +171,13 @@ const EventItem: FC<ListItemProps> = ({ playingVideo, item, showDetails, likeOrD
     .onEnd(() => likeOrDislikeMutation.mutate(item.id));
 
 
-  const handleLikeOrDislike = () => likeOrDislikeMutation.mutate(item.id);
-
   // play video on appear and pause on disappear
   useEffect(() => {
-    if (video.current) {
-      console.log('videoShouldPlay', videoShouldPlay);
-      console.log(playingVideo, item.id.toString()); // not printing the same value
+    if (videoRef.current) {
       if (videoShouldPlay) {
-        video.current.playAsync();
+        videoRef.current.playAsync();
       } else {
-        video.current.pauseAsync();
+        videoRef.current.pauseAsync();
       }
     }
   }, [videoShouldPlay]);
@@ -208,10 +201,9 @@ const EventItem: FC<ListItemProps> = ({ playingVideo, item, showDetails, likeOrD
             />
           ) : (
             <Video
-              ref={video}
+              ref={videoRef}
               useNativeControls
               resizeMode={ResizeMode.COVER}
-              // shouldPlay={videoShouldPlay}
               source={{ uri: item.mediaUrl }}
               style={{ width: '100%', height: '100%', overflow: 'hidden' }}
             // onPlaybackStatusUpdate={(status) => {
@@ -254,7 +246,7 @@ const EventItem: FC<ListItemProps> = ({ playingVideo, item, showDetails, likeOrD
               <FontAwesome name='share-square-o' size={34} style={{ color: tabIconDefault }} />
             )}
           </Pressable>
-          <Pressable onPressOut={() => showDetails(item)}>
+          <Pressable onPressOut={() => showEventDetails(item)}>
             {() => (
               <View
                 style={{
@@ -304,14 +296,8 @@ function arePropsEqual(prevProps: ListItemProps, nextProps: ListItemProps): bool
   // Implement your custom comparison logic here
   // Return true if the props are equal, return false if they are not
   return (
-    prevProps.layoutHeight === nextProps.layoutHeight &&
     prevProps.playingVideo === nextProps.playingVideo &&
-    prevProps.red100 === nextProps.red100 &&
-    prevProps.tabIconDefault === nextProps.tabIconDefault &&
-    prevProps.item.id === nextProps.item.id &&
-    prevProps.item.isLiked === nextProps.item.isLiked &&
-    prevProps.showDetails === nextProps.showDetails &&
-    prevProps.likeOrDislikeMutation === nextProps.likeOrDislikeMutation
+    prevProps.item.isLiked === nextProps.item.isLiked
   );
 }
 
