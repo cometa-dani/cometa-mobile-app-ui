f091e711f3382b0198f8c529c031aa9cdc90c3d7
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-12-21 11:28:21 -0500

    ✨ optimizing FlatList

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React, { FC, useRef, useState } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { LikedEvent, CreateEventLike } from '../../models/Event';
 import { StyleSheet, Image, DimensionValue, Pressable, SafeAreaView, Dimensions, ViewToken } from 'react-native';
 import { Text, View, useColors } from '../../components/Themed';
 import { GestureDetector, Gesture, FlatList, Directions } from 'react-native-gesture-handler';
 import { FontAwesome } from '@expo/vector-icons';
-import { router } from 'expo-router';
+import { router, useFocusEffect } from 'expo-router';
 import { useInfiniteQueryGetLatestEvents, useMutationLikeOrDislikeEvent } from '../../queries/eventHooks';
 import { UseMutationResult } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
@@ -13,147 +13,17 @@ import { useCometaStore } from '../../store/cometaStore';
 import { ResizeMode, Video } from 'expo-av';
 
 
-// Define the props for the memoized list item
-interface ListItemProps {
-  showDetails: (item: LikedEvent) => void,
-  videoShoulPlayOnAppear: boolean,
-  item: LikedEvent,
-  layoutHeight: DimensionValue,
-  red100: string,
-  tabIconDefault: string,
-  likeOrDislikeMutation: UseMutationResult<CreateEventLike | null, Error, number, void>
-}
-
-const EventItem: FC<ListItemProps> = ({ videoShoulPlayOnAppear, item, showDetails, likeOrDislikeMutation, layoutHeight, red100, tabIconDefault }) => {
-
-  // video
-  const video = React.useRef<Video>(null);
-
-  // can be lifted to the parent component like before
-  const swipeLeft = Gesture.Fling();
-  swipeLeft
-    .direction(Directions.LEFT)
-    .onStart(() => router.push('/bucketList'));
-
-  const doubleTap = Gesture.Tap();
-  doubleTap
-    .numberOfTaps(2)
-    .onEnd(() => likeOrDislikeMutation.mutate(item.id));
-
-  return (
-    <GestureDetector gesture={swipeLeft}>
-      <View style={{
-        alignItems: 'center',
-        height: layoutHeight,
-        justifyContent: 'center',
-        width: '100%'
-      }}>
-
-        {/* Background image or video */}
-        <GestureDetector gesture={doubleTap}>
-          {item.mediaType === 'IMAGE' ? (
-            <Image
-              source={{ uri: item.mediaUrl }}
-              style={{ width: '100%', height: '100%' }}
-            />
-          ) : (
-            <Video
-              ref={video}
-              useNativeControls
-              resizeMode={ResizeMode.COVER}
-              shouldPlay={videoShoulPlayOnAppear}
-              source={{ uri: item.mediaUrl }}
-              style={{ width: '100%', height: '100%', overflow: 'hidden' }}
-            />
-          )}
-        </GestureDetector>
-        {/* Background image or video */}
-
-        {/* Event title */}
-        <Text lightColor='#fff' darkColor='#eee' style={styles.title}>{item.name}</Text>
-
-        {/* Positioned buttons */}
-        <View lightColor='transparent' darkColor='transparent' style={styles.positionedButtons}>
-          <Pressable onPress={() => likeOrDislikeMutation.mutate(item.id)}>
-            {({ hovered, pressed }) => (
-              (item.isLiked) ? (
-                <FontAwesome name='heart' size={34} style={{ color: (hovered && pressed) ? tabIconDefault : red100 }} />
-              ) : (
-                <FontAwesome name='heart-o' size={34} style={{ color: (hovered && pressed) ? red100 : tabIconDefault }} />
-              )
-            )}
-          </Pressable>
-          <Pressable>
-            {() => (
-              <FontAwesome name='commenting-o' size={34} style={{ color: tabIconDefault }} />
-            )}
-          </Pressable>
-          <Pressable>
-            {() => (
-              <FontAwesome name='share-square-o' size={34} style={{ color: tabIconDefault }} />
-            )}
-          </Pressable>
-          <Pressable onPressOut={() => showDetails(item)}>
-            {() => (
-              <View
-                style={{
-                  borderRadius: 100,
-                  aspectRatio: 1,
-                  width: 34,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  backgroundColor: 'transparent',
-                  borderWidth: 3.4,
-                  borderColor: tabIconDefault
-                }}>
-                <FontAwesome name='chevron-down' size={22} style={{ color: tabIconDefault }} />
-              </View>
-            )}
-          </Pressable>
-        </View>
-        {/* Positioned buttons /*}
-
-      {/* Organizer icon */}
-        <View lightColor='transparent' darkColor='transparent' style={styles.organizerContainer}>
-          {item?.organization?.mediaUrl ? (
-            <Image style={styles.img} source={{ uri: item.organization?.mediaUrl }} />
-          ) : (
-            <Image style={styles.img} source={require('../../assets/images/icon.png')} />
-          )}
-          <Text
-            lightColor='#fff'
-            darkColor='#eee'
-            style={styles.organizer}>
-            {item.organization.name}
-          </Text>
-        </View>
-        {/* Organizer icon */}
-      </View>
-    </GestureDetector>
-  );
-};
-
-// function arePropsEqual(prevProps: ListItemProps, nextProps: ListItemProps): boolean {
-//   // Implement your custom comparison logic here
-//   // Return true if the props are equal, return false if they are not
-//   return (
-//     // prevProps.red100 === nextProps.red100 &&
-//     // prevProps.tabIconDefault === nextProps.tabIconDefault &&
-//     prevProps.item.isLiked === nextProps.item.isLiked &&
-//     prevProps.layoutHeight === nextProps.layoutHeight &&
-//     prevProps.likeOrDislikeMutation === nextProps.likeOrDislikeMutation
-//   );
-// }
-
-// const MemoizedEventItem = React.memo(EventItem, arePropsEqual);
-
-
 export default function HomeScreen(): JSX.Element {
+  // video
   const [playingVideo, setPlayingVideo] = useState<string>('');
 
   const onViewRef = useRef(({ viewableItems }: { viewableItems: ViewToken[] }) => {
     if (viewableItems.length > 0) {
-      setPlayingVideo(viewableItems[0].key);
+      // TODO: FIXE temporary fix to avoid playing multiple videos at the same time
+      console.log('viewableItems', viewableItems[0].item?.id);
+      setPlayingVideo(viewableItems[0].item?.id);
+    } else {
+      setPlayingVideo('');
     }
   });
 
@@ -173,10 +43,12 @@ export default function HomeScreen(): JSX.Element {
   // State variables to manage page and item heights
   const [layoutHeight, setLayoutHeight] = useState<DimensionValue>('100%');
 
-  const onShowEventDetails = (item: LikedEvent): void => {
+
+  const onShowEventDetails = useCallback((item: LikedEvent): void => {
     setLikedEvent(item);
     setToggleActionSheet(true);
-  };
+  }, [setLikedEvent, setToggleActionSheet]);
+
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -186,22 +58,27 @@ export default function HomeScreen(): JSX.Element {
       <View style={styles.container}>
         {/* Latest events list */}
         <FlatList
-          showsVerticalScrollIndicator={false}
-          pagingEnabled={true}
           onLayout={(e) => setLayoutHeight(e.nativeEvent.layout.height)}
           onViewableItemsChanged={onViewRef.current}
-          viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50 }}
+          viewabilityConfig={{ viewAreaCoveragePercentThreshold: 30 }}
+          keyExtractor={item => item.id.toString()}
+          removeClippedSubviews={true}
+          refreshing={isFetching}
+          initialNumToRender={2}
+          maxToRenderPerBatch={3}
+          showsVerticalScrollIndicator={false}
+          pagingEnabled={true}
           data={data?.pages.flatMap(page => page.events)}
           contentContainerStyle={styles.flatListContent}
           onEndReached={handleInfititeFetch}
           onEndReachedThreshold={1}
           renderItem={({ item }) => (
-            <EventItem
+            <MemoizedEventItem
               showDetails={onShowEventDetails}
               likeOrDislikeMutation={likeOrDislikeMutation}
               key={item.id}
               item={item}
-              videoShoulPlayOnAppear={String(item.id) === playingVideo}
+              playingVideo={playingVideo}
               layoutHeight={layoutHeight}
               red100={red100}
               tabIconDefault={tabIconDefault}
@@ -264,3 +141,178 @@ const styles = StyleSheet.create({
     top: 40
   }
 });
+
+
+// Define the props for the memoized list item
+interface ListItemProps {
+  showDetails: (item: LikedEvent) => void,
+  item: LikedEvent,
+  playingVideo: string,
+  layoutHeight: DimensionValue,
+  red100: string,
+  tabIconDefault: string,
+  likeOrDislikeMutation: UseMutationResult<CreateEventLike | null, Error, number, void>
+}
+
+const EventItem: FC<ListItemProps> = ({ playingVideo, item, showDetails, likeOrDislikeMutation, layoutHeight, red100, tabIconDefault }) => {
+
+  // video
+  const video = React.useRef<Video>(null);
+  // const videoShouldPlay = useMemo(() => playingVideo === item.id.toString(), [playingVideo]);
+  const videoShouldPlay = playingVideo === item.id.toString();
+
+  // can be lifted to the parent component like before
+  const swipeLeft = Gesture.Fling();
+  swipeLeft
+    .direction(Directions.LEFT)
+    .onStart(() => router.push('/bucketList'));
+
+
+  const doubleTap = Gesture.Tap();
+  doubleTap
+    .numberOfTaps(2)
+    .onEnd(() => likeOrDislikeMutation.mutate(item.id));
+
+
+  const handleLikeOrDislike = () => likeOrDislikeMutation.mutate(item.id);
+
+  // play video on appear and pause on disappear
+  useEffect(() => {
+    if (video.current) {
+      console.log('videoShouldPlay', videoShouldPlay);
+      console.log(playingVideo, item.id.toString()); // not printing the same value
+      if (videoShouldPlay) {
+        video.current.playAsync();
+      } else {
+        video.current.pauseAsync();
+      }
+    }
+  }, [videoShouldPlay]);
+
+
+  return (
+    <GestureDetector gesture={swipeLeft}>
+      <View style={{
+        alignItems: 'center',
+        height: layoutHeight,
+        justifyContent: 'center',
+        width: '100%'
+      }}>
+
+        {/* Background image or video */}
+        <GestureDetector gesture={doubleTap}>
+          {item.mediaType === 'IMAGE' ? (
+            <Image
+              source={{ uri: item.mediaUrl }}
+              style={{ width: '100%', height: '100%' }}
+            />
+          ) : (
+            <Video
+              ref={video}
+              useNativeControls
+              resizeMode={ResizeMode.COVER}
+              // shouldPlay={videoShouldPlay}
+              source={{ uri: item.mediaUrl }}
+              style={{ width: '100%', height: '100%', overflow: 'hidden' }}
+            // onPlaybackStatusUpdate={(status) => {
+            //   if (!status.isLoaded) {
+            //     // Error handling
+            //   } else {
+            //     if (status.isPlaying && !videoShouldPlay) {
+            //       video.current?.pauseAsync();
+            //     } else if (!status.isPlaying && videoShouldPlay) {
+            //       video.current?.playAsync();
+            //     }
+            //   }
+            // }}
+            />
+          )}
+        </GestureDetector>
+        {/* Background image or video */}
+
+        {/* Event title */}
+        <Text lightColor='#fff' darkColor='#eee' style={styles.title}>{item.name}</Text>
+
+        {/* Positioned buttons */}
+        <View lightColor='transparent' darkColor='transparent' style={styles.positionedButtons}>
+          <Pressable onPress={handleLikeOrDislike}>
+            {({ hovered, pressed }) => (
+              (item.isLiked) ? (
+                <FontAwesome name='heart' size={34} style={{ color: (hovered && pressed) ? tabIconDefault : red100 }} />
+              ) : (
+                <FontAwesome name='heart-o' size={34} style={{ color: (hovered && pressed) ? red100 : tabIconDefault }} />
+              )
+            )}
+          </Pressable>
+          <Pressable>
+            {() => (
+              <FontAwesome name='commenting-o' size={34} style={{ color: tabIconDefault }} />
+            )}
+          </Pressable>
+          <Pressable>
+            {() => (
+              <FontAwesome name='share-square-o' size={34} style={{ color: tabIconDefault }} />
+            )}
+          </Pressable>
+          <Pressable onPressOut={() => showDetails(item)}>
+            {() => (
+              <View
+                style={{
+                  borderRadius: 100,
+                  aspectRatio: 1,
+                  width: 34,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: 'transparent',
+                  borderWidth: 3.4,
+                  borderColor: tabIconDefault
+                }}>
+                <FontAwesome name='chevron-down' size={22} style={{ color: tabIconDefault }} />
+              </View>
+            )}
+          </Pressable>
+        </View>
+        {/* Positioned buttons /*}
+
+      {/* Organizer icon */}
+        <View lightColor='transparent' darkColor='transparent' style={styles.organizerContainer}>
+          {item?.organization?.mediaUrl ? (
+            <Image style={styles.img} source={{ uri: item.organization?.mediaUrl }} />
+          ) : (
+            <Image style={styles.img} source={require('../../assets/images/icon.png')} />
+          )}
+          <Text
+            lightColor='#fff'
+            darkColor='#eee'
+            style={styles.organizer}>
+            {item.organization.name}
+          </Text>
+        </View>
+        {/* Organizer icon */}
+      </View>
+    </GestureDetector>
+  );
+};
+
+/**
+ * 
+ * @description This function is used to compare the props of the list item.
+ * If false the list item will be re-rendered, otherwise it will not 
+ * @returns boolean 
+ */
+function arePropsEqual(prevProps: ListItemProps, nextProps: ListItemProps): boolean {
+  // Implement your custom comparison logic here
+  // Return true if the props are equal, return false if they are not
+  return (
+    prevProps.layoutHeight === nextProps.layoutHeight &&
+    prevProps.playingVideo === nextProps.playingVideo &&
+    prevProps.red100 === nextProps.red100 &&
+    prevProps.tabIconDefault === nextProps.tabIconDefault &&
+    prevProps.item.id === nextProps.item.id &&
+    prevProps.item.isLiked === nextProps.item.isLiked &&
+    prevProps.showDetails === nextProps.showDetails &&
+    prevProps.likeOrDislikeMutation === nextProps.likeOrDislikeMutation
+  );
+}
+
+const MemoizedEventItem = React.memo(EventItem, arePropsEqual);
