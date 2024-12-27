12adfe8e8bf75610ff37c38e97923f1b8dba409a
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-12-27 12:00:24 -0500

    ✨ bottomsheet working fine

--- components/userProfile/targetUserProfile.tsx
+++ components/userProfile/targetUserProfile.tsx
@@ -1,4 +1,4 @@
-import { FlashList } from '@shopify/flash-list';
+import { FlashList, MasonryFlashList } from '@shopify/flash-list';
 import { createRef, FC, forwardRef, RefObject, useCallback } from 'react';
 import { UnistylesRuntime, useStyles } from 'react-native-unistyles';
 import { tabBarHeight } from '../tabBar/tabBar';
@@ -13,7 +13,7 @@ import { IGetLatestPaginatedEvents } from '@/models/Event';
 import { Heading } from '../text/heading';
 import { TextView } from '../text/text';
 import { Image } from 'expo-image';
-import DefaultBottomSheet, { BottomSheetScrollView, BottomSheetView } from '@gorhom/bottom-sheet';
+import DefaultBottomSheet, { BottomSheetFlatList, BottomSheetScrollView, BottomSheetView } from '@gorhom/bottom-sheet';
 import { useQueryGetTargetUserPeopleProfile } from '@/queries/targetUser/userProfileHooks';
 import { useInfiniteQueryGetSameMatchedEventsByTwoUsers, useInfiniteQueryGetTargetUserBucketList } from '@/queries/targetUser/eventHooks';
 import { create } from 'zustand';
@@ -59,19 +59,19 @@ export const TargetUserProfile: FC = () => {
       )) || []
   );
   console.log(userUuid);
-  // const matchesEvents: IBucketListItem[] = (
-  //   matches.data?.pages.
-  //     flatMap(({ items: events }) => (
-  //       events.map(
-  //         item => ({
-  //           id: item?.photos[0]?.id,
-  //           img: item?.photos[0]?.url,
-  //           placeholder: item?.photos[0]?.placeholder,
-  //           location: item?.location?.name,
-  //         })
-  //       )
-  //     )) || []
-  // );
+  const matchesEvents: IBucketListItem[] = (
+    matches.data?.pages.
+      flatMap(({ items: events }) => (
+        events.map(
+          item => ({
+            id: item?.photos[0]?.id,
+            img: item?.photos[0]?.url,
+            placeholder: item?.photos[0]?.placeholder,
+            location: item?.location?.name,
+          })
+        )
+      )) || []
+  );
 
   const UserHeader: FC = useCallback(() => (
     !userProfile.isSuccess ? (
@@ -112,44 +112,80 @@ export const TargetUserProfile: FC = () => {
       keyboardBehavior="fillParent"   // don't change
       snapPoints={snapPoints}
     >
-      {/* <ScrollView
-        contentContainerStyle={{
-          paddingTop: theme.spacing.sp7,
-          paddingBottom: tabBarHeight * 2,
-        }}>
 
-
-      </ScrollView> */}
-      <SafeAreaView
-        edges={{ bottom: 'off', top: 'additive' }}
-        style={{ flex: 1, backgroundColor: theme.colors.white80 }}
-      >
-        <BottomSheetScrollView style={{
+      {/* <BottomSheetScrollView style={{
           // paddingVertical: theme.spacing.sp7,
           // paddingHorizontal: theme.spacing.sp10,
         }}>
-          <UserHeader />
 
-          <FlashList
-            data={!bucketList.isSuccess ? dummyBucketListItems : bucketListEvents}
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            pagingEnabled={true}
-            estimatedItemSize={UnistylesRuntime.screen.height * 0.2}
-            onEndReachedThreshold={0.4}
-            // onEndReached={onBucketListEndReached}
-            renderItem={renderBucketItem}
-          />
-          <Heading size='s6' style={{
-            paddingHorizontal: theme.spacing.sp6,
-            paddingBottom: theme.spacing.sp1,
-            paddingTop: theme.spacing.sp6
-          }}>
-            Matches
-          </Heading>
-        </BottomSheetScrollView>
+        </BottomSheetScrollView> */}
+
+
+      <SafeAreaView
+        edges={{ bottom: 'off' }}
+        style={{
+          flex: 1,
+          backgroundColor: theme.colors.white80,
+          paddingVertical: theme.spacing.sp7
+        }}>
+        <BottomSheetFlatList
+          data={matchesEvents}
+          ListHeaderComponent={() => (
+            <BottomSheetView>
+              <UserHeader />
+
+              <FlashList
+                data={!bucketList.isSuccess ? dummyBucketListItems : bucketListEvents}
+                showsHorizontalScrollIndicator={false}
+                horizontal={true}
+                pagingEnabled={true}
+                estimatedItemSize={UnistylesRuntime.screen.height * 0.2}
+                onEndReachedThreshold={0.4}
+                // onEndReached={onBucketListEndReached}
+                renderItem={renderBucketItem}
+              />
+              <Heading size='s6' style={{
+                paddingHorizontal: theme.spacing.sp6,
+                paddingBottom: theme.spacing.sp1,
+                paddingTop: theme.spacing.sp6
+              }}>
+                Matches
+              </Heading>
+            </BottomSheetView>
+            // </SafeAreaView>
+          )}
+          style={{ flex: 1 }}
+          // contentContainerStyle={{
+          //   paddingHorizontal: theme.spacing.sp6,
+          //   paddingVertical: theme.spacing.sp7
+          // }}
+          numColumns={3}
+          ItemSeparatorComponent={() => <View style={{ height: theme.spacing.sp2 }} />}
+          columnWrapperStyle={{
+            gap: theme.spacing.sp2
+          }}
+          renderItem={(item) => (
+            <View
+              key={item?.item?.id}
+              style={{
+                height: UnistylesRuntime.screen.height * 0.2,
+                flex: 1 / 3,
+                flexDirection: 'row',
+              }}
+            >
+              <Image
+                source={{ uri: item?.item?.img }}
+                placeholder={{ thumbhash: item?.item?.placeholder }}
+                style={{
+                  width: '100%',
+                  flex: 1,
+                  borderRadius: theme.spacing.sp4
+                }}
+              />
+            </View>
+          )}
+        />
       </SafeAreaView>
-
       {/* <FlatList
         data={dummyBucketListItems}
         // showsHorizontalScrollIndicator={false}
@@ -185,46 +221,6 @@ export const TargetUserProfile: FC = () => {
 };
 
 // TargetUserProfile.displayName = 'TargetUserProfile';
-
-{/* <MasonryFlashList
-        data={dummyBucketListItems}
-        showsHorizontalScrollIndicator={false}
-        numColumns={3}
-        estimatedItemSize={UnistylesRuntime.screen.height * 0.2}
-        // contentContainerStyle={{
-        //   paddingHorizontal: theme.spacing.sp6,
-        //   paddingTop: theme.spacing.sp7,
-        //   paddingBottom: tabBarHeight * 2
-        // }}
-        // ListHeaderComponent={() => (
-        //   <>
-
-        //   </>
-        // )}
-        // keyExtractor={item => item?.id?.toString()}
-        ItemSeparatorComponent={() => <View style={{ height: theme.spacing.sp2 }} />}
-        // onEndReachedThreshold={0.4}
-        // onEndReached={onBucketListEndReached}
-        renderItem={({ columnIndex, item }) => (
-          <View style={{
-            // marginLeft: columnIndex === 0 ? 0 : theme.spacing.sp2, // Add spacing for other columns
-            height: UnistylesRuntime.screen.height * 0.2,
-          }}>
-            <Image
-              source={{ uri: item?.img }}
-              placeholder={{ thumbhash: item?.placeholder }}
-              style={{
-                height: UnistylesRuntime.screen.height * 0.2,
-                // width: 'auto',
-                borderRadius: theme.spacing.sp4
-              }}
-              contentFit='cover'
-              transition={imageTransition}
-              recyclingKey={item?.img}
-            />
-          </View>
-        )}
-      /> */}
 
 
 type DefaultBottomSheetProps = {
