345c96667010f43b14ae4300b69312e7b93b1b95
cesaraugusto316 <cesaraugusto316@protonmail.com>
2025-04-20 20:20:37 -0500

    ✨ updates

--- queries/notification/useNotifications.ts
+++ queries/notification/useNotifications.ts
@@ -1,6 +1,6 @@
 import { useCometaStore } from '@/store/cometaStore';
 import { supabase } from '@/supabase/config';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { QueryKeys } from '../queryKeys';
 import { useEffect } from 'react';
 import notificationService from '@/services/notificationService';
@@ -8,7 +8,6 @@ import { INotification } from '@/models/Notification';
 
 
 export const useNotifications = (limit = 100) => {
-  const queryClient = useQueryClient();
   const currentUser = useCometaStore(state => state.userProfile);
 
   // Fetch initial messages
@@ -31,60 +30,8 @@ export const useNotifications = (limit = 100) => {
   // Subscribe to real-time updates
   useEffect(() => {
     if (!currentUser?.id) return;
-
-    // const receiverChannel = supabase
-    //   .channel('notifications_receiver')
-    //   .on<INotification>(
-    //     'postgres_changes',
-    //     {
-    //       schema: 'public',
-    //       event: 'INSERT',
-    //       table: 'Notification',
-    //       filter: `receiver_id=eq.${currentUser.id}`
-    //     },
-    //     (payload) => {
-    //       queryClient.setQueryData<INotification[]>([QueryKeys.GET_NOTIFICATIONS, currentUser.id], (oldData) => {
-    //         if (!oldData) return [];
-    //         // insert
-    //         return [payload.new, ...oldData];
-    //       });
-    //     }
-    //   )
-    //   // .on<INotification>(
-    //   //   'postgres_changes',
-    //   //   {
-    //   //     schema: 'public',
-    //   //     event: 'UPDATE',
-    //   //     table: 'Notification',
-    //   //     filter: `receiver_id=eq.${currentUser.id}`
-    //   //   },
-    //   //   (payload) => {
-    //   //     queryClient.setQueryData<INotification[]>([QueryKeys.GET_NOTIFICATIONS, currentUser.id], (oldData) => {
-    //   //       if (!oldData) return [];
-    //   //       // update
-    //   //       return oldData.map(notification => {
-    //   //         if (notification.id === payload.new.id) {
-    //   //           return payload.new;
-    //   //         }
-    //   //         return notification;
-    //   //       });
-    //   //     });
-    //   //   }
-    //   // )
-    //   .on<INotification>(
-    //     'postgres_changes',
-    //     {
-    //       schema: 'public',
-    //       event: 'DELETE',
-    //       table: 'Notification',
-    //       filter: `receiver_id=eq.${currentUser.id}`
-    //     },
-    //     () => query.refetch()
-    //   )
-    //   .subscribe();
-
-    const notificationsChannel = supabase
-      .channel('notifications')
+    const insertNotificationsChannel = supabase
+      .channel('notifications_insert')
       .on<INotification>(
         'postgres_changes',
         {
@@ -93,40 +40,15 @@ export const useNotifications = (limit = 100) => {
           table: 'Notification',
           filter: `user_id=eq.${currentUser.id}`
         },
-        (payload) => {
-          queryClient.setQueryData<INotification[]>([QueryKeys.GET_NOTIFICATIONS, currentUser.id], (oldData) => {
-            if (!oldData) return [];
-            // insert
-            return [payload.new, ...oldData];
-          });
-        }
+        () => query.refetch()
       )
-      // .subscribe()
-      // .on<INotification>(
-      //   'postgres_changes',
-      //   {
-      //     schema: 'public',
-      //     event: 'UPDATE',
-      //     table: 'Notification',
-      //     filter: `sender_id=eq.${currentUser.id}`
-      //   },
-      //   (payload) => {
-      //     queryClient.setQueryData<INotification[]>([QueryKeys.GET_NOTIFICATIONS, currentUser.id], (oldData) => {
-      //       if (!oldData) return [];
-      //       // update
-      //       return oldData.map(notification => {
-      //         if (notification.id === payload.new.id) {
-      //           return payload.new;
-      //         }
-      //         return notification;
-      //       });
-      //     });
-      //   }
-      // )
       .on<INotification>(
         'postgres_changes',
         {
           schema: 'public',
+          // important
+          // this works with supabase sql editor:
+          // ALTER TABLE public."Notification" REPLICA IDENTITY FULL;
           event: 'DELETE',
           table: 'Notification',
           filter: `user_id=eq.${currentUser.id}`
@@ -136,8 +58,7 @@ export const useNotifications = (limit = 100) => {
       .subscribe();
 
     return () => {
-      // supabase.removeChannel(receiverChannel);
-      supabase.removeChannel(notificationsChannel);
+      supabase.removeChannel(insertNotificationsChannel);
     };
   }, [currentUser?.id]);
 
