bacb05876848df1661caf7d81a8b34723c6696de
cesaraugusto316 <cesaraugusto316@protonmail.com>
2025-04-27 18:45:05 -0500

    ✨ loading indicator

--- app/(companyTabs)/index.tsx
+++ app/(companyTabs)/index.tsx
@@ -1,17 +1,20 @@
 import { tabBarHeight } from '@/components/tabBar/tabBar';
 import { EventItem, IBucketListItem } from '@/components/userProfile/components/eventItem';
+import { Condition } from '@/components/utils/ifElse';
+import { Center } from '@/components/utils/stacks';
 import { useQueryGetEventsPaginated } from '@/queries/organization/eventHooks';
 import { useQueryGetCompanyProfile } from '@/queries/organization/organizationHooks';
 import { FlashList } from '@shopify/flash-list';
 import { useCallback } from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
+import { SystemBars } from 'react-native-edge-to-edge';
 import { createStyleSheet, UnistylesRuntime, useStyles } from 'react-native-unistyles';
 
 
 export default function HomeScreen() {
   const { theme } = useStyles(stylesheet);
   useQueryGetCompanyProfile();
-  const { data: events } = useQueryGetEventsPaginated();
+  const { data: events, isSuccess } = useQueryGetEventsPaginated();
 
   const renderBucketItem = useCallback(({ item }: { item: IBucketListItem }) => (
     <EventItem item={item} />
@@ -28,19 +31,38 @@ export default function HomeScreen() {
   );
 
   return (
-    <FlashList
-      data={eventsList}
-      showsVerticalScrollIndicator={false}
-      estimatedItemSize={UnistylesRuntime.screen.height * 0.2}
-      contentContainerStyle={{
-        paddingVertical: theme.spacing.sp6,
-        paddingHorizontal: theme.spacing.sp4
-      }}
-      ListFooterComponentStyle={{ height: tabBarHeight * 2 }}
-      ItemSeparatorComponent={() => <View style={{ height: theme.spacing.sp6 }} />}
-      // onEndReachedThreshold={0.4}
-      renderItem={renderBucketItem}
-    />
+    <>
+      <SystemBars style='auto' />
+      <Condition
+        if={isSuccess}
+        then={(
+          <View style={{ flex: 1, position: 'relative' }}>
+            <FlashList
+              data={eventsList}
+              showsVerticalScrollIndicator={false}
+              estimatedItemSize={UnistylesRuntime.screen.height * 0.2}
+              contentContainerStyle={{
+                paddingVertical: theme.spacing.sp6,
+                paddingHorizontal: theme.spacing.sp4
+              }}
+              ListFooterComponentStyle={{ height: tabBarHeight * 3 }}
+              ItemSeparatorComponent={() => <View style={{ height: theme.spacing.sp6 }} />}
+              renderItem={renderBucketItem}
+            />
+          </View>
+        )}
+        else={(
+          <Center styles={{ flex: 1 }}>
+            <ActivityIndicator
+              size="large"
+              style={{ marginTop: -theme.spacing.sp8 }}
+              color={theme.colors.red100}
+            />
+          </Center>
+        )}
+      />
+    </>
+    // eslint-disable-next-line react-native/no-inline-styles,react-native/no-color-literals
   );
 }
 
